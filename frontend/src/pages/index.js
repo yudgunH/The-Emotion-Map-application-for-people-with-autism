f466ee801fac343b51a721361e6819122a832e7b
Nguyễn Duy Hưng <124774263+yudgunH@users.noreply.github.com>
2024-11-21 21:38:49 +0700

    Set Up NextJS

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,10 @@
+import MainLayout from "@/layouts/MainLayout";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <h2>Welcome to the Home Page</h2>
+      <p>Hello World from Home Page!</p>
+    </MainLayout>
+  );
+}
