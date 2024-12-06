d0f954e7c946694f9a156601287bf61f96398ca3
phung1003 <anhungtrnc@gmail.com>
2024-12-06 09:53:36 +0700

    api sửa r nè

--- backend/main.py
+++ backend/main.py
@@ -56,57 +56,62 @@ def extract_image_from_request(img_key: str) -> Union[str, np.ndarray]:
     raise ValueError(f"'{img_key}' not found in request in either json or form data")
 
 
-@flask.route("/represent", methods=["POST"])
-def represent():
-    input_args = request.get_json() or request.form.to_dict()
+# built-in dependencies
+from typing import Union
 
-    try:
-        img = extract_image_from_request("img")
-    except Exception as err:
-        return {"exception": str(err)}, 400
+# 3rd party dependencies
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
 
-    obj = service.represent(
-        img_path=img,
-        model_name=input_args.get("model_name", "VGG-Face"),
-        detector_backend=input_args.get("detector_backend", "opencv"),
-        enforce_detection=input_args.get("enforce_detection", True),
-        align=input_args.get("align", True),
-        anti_spoofing=input_args.get("anti_spoofing", False),
-        max_faces=input_args.get("max_faces"),
-    )
+# project dependencies
+from deepface import DeepFace
+from deepface.commons import image_utils
 
 
-    return obj
 
 
-@flask.route("/verify", methods=["POST"])
-def verify():
-    input_args = request.get_json() or request.form.to_dict()
+flask = Flask(__name__)
+CORS(flask)
 
-    try:
-        img1 = extract_image_from_request("img1")
-    except Exception as err:
-        return {"exception": str(err)}, 400
+# pylint: disable=no-else-return, broad-except
 
-    try:
-        img2 = extract_image_from_request("img2")
-    except Exception as err:
-        return {"exception": str(err)}, 400
 
-    verification = service.verify(
-        img1_path=img1,
-        img2_path=img2,
-        model_name=input_args.get("model_name", "VGG-Face"),
-        detector_backend=input_args.get("detector_backend", "opencv"),
-        distance_metric=input_args.get("distance_metric", "cosine"),
-        align=input_args.get("align", True),
-        enforce_detection=input_args.get("enforce_detection", True),
-        anti_spoofing=input_args.get("anti_spoofing", False),
-    )
 
-    logger.debug(verification)
 
-    return verification
+def extract_image_from_request(img_key: str) -> Union[str, np.ndarray]:
+
+    if request.files:
+        # request.files is instance of werkzeug.datastructures.ImmutableMultiDict
+        # file is instance of werkzeug.datastructures.FileStorage
+        file = request.files.get(img_key)
+
+        if file is None:
+            raise ValueError(f"Request form data doesn't have {img_key}")
+
+        if file.filename == "":
+            raise ValueError(f"No file uploaded for '{img_key}'")
+
+        img = image_utils.load_image_from_file_storage(file)
+
+        return img
+    # Check if the request is coming as base64, file path or url from json or form data
+    elif request.is_json or request.form:
+        input_args = request.get_json() or request.form.to_dict()
+
+        if input_args is None:
+            raise ValueError("empty input set passed")
+
+        # this can be base64 encoded image, and image path or url
+        img = input_args.get(img_key)
+
+        if not img:
+            raise ValueError(f"'{img_key}' not found in either json or form data request")
+
+        return img
+
+    # If neither JSON nor file input is present
+    raise ValueError(f"'{img_key}' not found in request in either json or form data")
 
 
 @flask.route("/analyze", methods=["POST"])
@@ -118,20 +123,28 @@ def analyze():
     except Exception as err:
         return {"exception": str(err)}, 400
 
-    # actions = input_args.get("actions", ["age", "gender", "emotion", "race"])
-   
+    try:
+        demographies = DeepFace.analyze(
+            img_path=img,
+            actions="emotion",
+            detector_backend=input_args.get("detector_backend", "opencv"),
+            enforce_detection=input_args.get("enforce_detection", True),
+            align=input_args.get("align", True),
+            anti_spoofing=input_args.get("anti_spoofing", False),
+        )
 
-    demographies = DeepFace.analyze(
-        img_path=img,
-        actions= "emotion",
-        detector_backend=input_args.get("detector_backend", "opencv"),
-        enforce_detection=input_args.get("enforce_detection", True),
-        align=input_args.get("align", True),
-        anti_spoofing=input_args.get("anti_spoofing", False),
-    )
+        # Extract the dominant emotion
+        dominant_emotion = demographies[0]['dominant_emotion']
+        return str(dominant_emotion), 200
 
-    print(str(demographies))
-    return str(demographies)
+    except Exception as err:
+        return {"exception": str(err)}, 500
+
+
+
+if __name__ == "__main__":
+    flask.run(debug=True, port=5005)
+
 
 
 if __name__ == "__main__":
